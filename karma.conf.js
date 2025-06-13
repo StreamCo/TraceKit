05768eef9931b214418427f7101bfb2b49efcea5
Pablo Vieira <pablo.vieira@stan.com.au>
2025-06-13 10:53:00 +1000

    karma: Update libs to use ChromeHeadlessNew instead of PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -1,64 +1,61 @@
 // Karma configuration
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
+    basePath: "",
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
+    frameworks: ["jasmine"],
 
     // list of files / patterns to load in the browser
-    files: [
-      'tracekit.js',
-      'spec/**/*.js'
-    ],
-
+    files: ["tracekit.js", "spec/**/*.js"],
 
     // list of files to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
+    preprocessors: {},
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ["progress"],
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ["ChromeHeadlessNew"],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  })
+    singleRun: false,
+    // Configure Karma plugins and custom browser launchers
+    plugins: ["karma-jasmine", "karma-chrome-launcher"],
+    customLaunchers: {
+      ChromeHeadlessNew: {
+        base: "ChromeHeadless",
+        // Flag for running Chrome in CI/Docker environments
+        flags: ["--no-sandbox"],
+        puppeteerOptions: {
+          // Opt-in to Puppeteer's new headless mode for Chrome
+          headless: "new",
+        },
+      },
+    },
+  });
 };
